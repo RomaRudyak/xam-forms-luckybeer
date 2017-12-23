0c1d158b0fe207ab2f111cc83f8ff8f827887240
Roman Rudyak <roma.rudyak@gmail.com>
2017-12-24 00:18:36 +0200

    add stake kit for views

---
+++ LuckyBeer/StateKit/StateCondition.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace LuckyBeer.StateKit
+{
+    [ContentProperty("Content")]
+    public class StateCondition : View
+    {
+        public object State { get; set; }
+        public View Content { get; set; }
+    }
+}
